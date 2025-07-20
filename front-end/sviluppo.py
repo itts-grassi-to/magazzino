b7c0d6952b2db080908a7ef2f84e733856d136d1
Daniele Ortu <daniele.ortu@itisgrassi.edu.it>
2025-07-20 17:33:07 +0200

    inserimento di sviluppo.py

---
+++ front-end/sviluppo.py
@@ -0,0 +1,14 @@
+
+SVILUPPO=False
+#logato = { }
+if SVILUPPO:
+    import os
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    project_root = os.path.dirname(current_dir)
+    target_module_dir = os.path.join(project_root, 'utilita')
+    if target_module_dir not in os.sys.path:
+        os.sys.path.append(target_module_dir)
+    target_module_dir = os.path.join(project_root, 'img')
+    if target_module_dir not in os.sys.path:
+        os.sys.path.append(target_module_dir)
+
